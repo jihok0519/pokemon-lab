37aeb78aeae7b81e32ab1a6b69c4ccf361611380
Jiho Kim <jihok0519@gmail.com>
2022-05-19 16:23:40 -0400

    set up all routes

--- server.js
+++ server.js
@@ -3,22 +3,56 @@ const app = express();
 const pokemon = require('./models/pokemon.js');
 const port = 3000;
 
-// Index
+app.use(express.urlencoded({ extended: false }));
 
+
+// Index
 app.get("/pokemon/", (req, res) => {
     res.render("index.ejs", {
         allPokemon: pokemon,
     });
 });
 
-// Show
+// New
+app.get("/pokemon/new/", (req, res) => {
+    res.render("new.ejs");
+});
 
+// Delete
+app.delete("/pokemon/:id/", (req, res) => {
+    pokemon.splice(req.params.index, 1);
+	//redirect back to index route
+	res.redirect("/pokemon/");
+});
+
+// Update
+app.put("/pokemon/:id/", (req, res) => {
+	pokemon[req.params.id] = req.body;
+	//redirect to the index page
+	res.redirect("/pokemon/");
+});
+
+// Create
+app.post("/pokemon/", (req, res) => {
+	pokemon.push(req.body);
+	res.redirect("/pokemon/");
+});
+
+// Edit
+app.get("/pokemon/:id/edit", (req, res) => {
+	res.render("edit.ejs", {
+		pokemons: pokemon[req.params.id],
+		index: req.params.id,
+	});
+});
+
+// Show
 app.get("/pokemon/:id/", (req, res) => {
     res.render("show.ejs", {
         pokemons: pokemon[req.params.id],
-    })
-})
+    });
+});
 
 app.listen(port, () => {
-    console.log("listen on port", port);
-})
+    console.log("listen on port", port)
+});
